e89e9740e8e7561bce913d0600b600cb06ec091f
Crowdbotics <team@crowdbotics.com>
2021-08-07 14:22:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "broad_field_29607Identifier",
+  url: "https://broad_field_29607Identifier.botics.co",
+  api: "https://broad_field_29607Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
